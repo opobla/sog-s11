f2b9b5f69f8812116ddd7832e51f05cadbda2eac
Óscar García <oscar.garcia@makingscience.com>
2024-11-18 13:05:52 +0100

    Step

--- play_process.c
+++ play_process.c
@@ -2,8 +2,18 @@
 #include <stdio.h>
 
 int main() {
+    int pid;
 
-    fork();
-    printf("I am a process with pid %d \n", getpid());
+    pid = fork();
+    if (pid == 0) {
+        // This code is to be executed by the child
+        printf("I am the child with pid %d and my father %d, and pid=%d \n", getpid(), getppid(), pid);
+    } else {
+        // This code is to be executed by the father
+        printf("I am the father with pid %d and my father %d, and pid=%d \n", getpid(), getppid(), pid);
+    }
+
+
+    sleep(1);
 
 }
